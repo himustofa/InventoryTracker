ea4db7ca9b5fc9f2518408fad61f5ec160737cb2
himustofa <mustofa2008@gmail.com>
2020-01-04 13:50:57 +0600

    export to excel file

--- app/src/main/java/com/appsit/inventorytracker/utils/ConstantKey.java
+++ app/src/main/java/com/appsit/inventorytracker/utils/ConstantKey.java
@@ -3,6 +3,6 @@ package com.appsit.inventorytracker.utils;
 public class ConstantKey {
 
     public final static String DATABASE_NAME = "inventory_tracker";
-    public final static int DATABASE_VERSION = 2;
+    public final static int DATABASE_VERSION = 1;
 
 }
